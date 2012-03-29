f7c27e84bdb76322bd65e3e6bb2619a47c4ebb43
Hoàng Minh Thắng <hoangminhthang@ktqd.org>
2012-03-29 12:34:40 +0700

    auto remove all old version when installing

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 # It automates building and installing the development version of the lein-noir-gen.
 
 lein uberjar
+rm ~/.lein/plugins/lein-noir-gen-*.jar
 mv lein-noir-gen-0.3.0-standalone.jar ~/.lein/plugins/lein-noir-gen-0.3.0.jar
-echo "Noir-gen successfully installed!"
+echo "Noir-gen successfully installed/upgraded!"
