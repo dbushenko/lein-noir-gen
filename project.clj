4fcb33e9a2c1f7235b0aa2b7d63acad4c520714f
Hoàng Minh Thắng <hoangminhthang@ktqd.org>
2012-03-29 13:18:47 +0700

    Add midje test

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject lein-noir-gen "0.3.0"
   :description "Noir CRUD generator"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [de.ubercode.clostache/clostache "1.1.0"]]
+                 [de.ubercode.clostache/clostache "1.1.0"]
+                 [midje "1.3.1"]]
   :eval-in-leiningen true
   :main leiningen.noir-gen)
---
+++ test/leiningen/test/noir_gen.clj
@@ -0,0 +1,8 @@
+(ns leiningen.test.noir-gen
+  (:use [leiningen.noir-gen])
+  (:use [clojure.test])
+  (:use [midje.sweet]))
+
+(facts
+0
+=> 0)
